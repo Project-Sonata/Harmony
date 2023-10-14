ea822762f0fa6c603fb21aceae6b1aaae4bfd0f3
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-14 19:47:02 +0300

    Root class

---
+++ src/test/java/com/odeyalo/sonata/harmony/HarmonyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.odeyalo.sonata.harmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HarmonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
