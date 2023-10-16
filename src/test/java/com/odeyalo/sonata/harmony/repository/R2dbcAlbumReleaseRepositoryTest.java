71b478c20c23867c5e03bbdd0de0be3241522ff2
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-16 20:31:07 +0300

    Added configuration to wrap the beans

--- src/test/java/com/odeyalo/sonata/harmony/repository/R2dbcAlbumReleaseRepositoryTest.java
+++ src/test/java/com/odeyalo/sonata/harmony/repository/R2dbcAlbumReleaseRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.odeyalo.sonata.harmony.repository;
 
 import com.odeyalo.sonata.harmony.entity.AlbumReleaseEntity;
+import com.odeyalo.sonata.harmony.repository.r2dbc.delegate.R2dbcAlbumReleaseRepositoryDelegate;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import reactor.test.StepVerifier;
 
@@ -15,6 +19,7 @@ import static com.odeyalo.sonata.harmony.entity.AlbumReleaseEntity.builder;
 @DataR2dbcTest
 @TestInstance(Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
+@Import(R2dbcAlbumReleaseRepositoryTest.Configuration.class)
 public class R2dbcAlbumReleaseRepositoryTest {
 
     @Autowired
@@ -47,5 +52,14 @@ public class R2dbcAlbumReleaseRepositoryTest {
 
     private static AlbumReleaseEntity createValidAlbumWithEmptyId() {
         return builder().albumName("dudeness").build();
+    }
+
+    @TestConfiguration
+    public static class Configuration {
+
+        @Bean
+        public R2dbcAlbumReleaseRepository r2dbcAlbumReleaseRepository(R2dbcAlbumReleaseRepositoryDelegate delegate) {
+            return new R2dbcAlbumReleaseRepository(delegate);
+        }
     }
 }
