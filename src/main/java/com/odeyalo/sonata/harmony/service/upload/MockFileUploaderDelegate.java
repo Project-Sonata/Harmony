d24bdd433a620e4ba531dbf824b408eab5766df1
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-25 22:09:45 +0300

    FileUploaderDelegate that returns predefined values

--- src/main/java/com/odeyalo/sonata/harmony/service/upload/MockFileUploaderDelegate.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/upload/MockFileUploaderDelegate.java
@@ -2,10 +2,18 @@ package com.odeyalo.sonata.harmony.service.upload;
 
 import reactor.core.publisher.Mono;
 
+/**
+ * FileUploaderDelegate that returns predefined values
+ */
 public class MockFileUploaderDelegate implements FileUploaderDelegate {
+    private final FileUrl result;
+
+    public MockFileUploaderDelegate(FileUrl result) {
+        this.result = result;
+    }
 
     @Override
     public Mono<FileUrl> uploadFile(FileUploadTarget file) {
-        return Mono.just(FileUrl.urlOnly("something"));
+        return Mono.just(result);
     }
 }
