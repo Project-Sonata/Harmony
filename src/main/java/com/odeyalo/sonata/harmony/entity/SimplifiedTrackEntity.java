02669091693af0c7d1c26ed69990283e93bcbc87
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-30 19:14:30 +0200

    Simplified version of Track entity that does not contain album

---
+++ src/main/java/com/odeyalo/sonata/harmony/entity/SimplifiedTrackEntity.java
@@ -0,0 +1,40 @@
+package com.odeyalo.sonata.harmony.entity;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * Simplified version of Track entity that does not contain album
+ */
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@SuperBuilder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table("tracks")
+public class SimplifiedTrackEntity implements ArtistContainerHolder {
+    @Id
+    Long id;
+    String name;
+    @Column("duration_ms")
+    Long durationMs;
+    @Column("is_explicit")
+    boolean explicit;
+    @Column("has_lyrics")
+    @Accessors(fluent = true)
+    boolean hasLyrics;
+    @Column("disc_number")
+    Integer discNumber;
+    @Column("index")
+    Integer index;
+    @Column("album_id")
+    Long albumId;
+    @Transient
+    ArtistContainerEntity artists;
+}
