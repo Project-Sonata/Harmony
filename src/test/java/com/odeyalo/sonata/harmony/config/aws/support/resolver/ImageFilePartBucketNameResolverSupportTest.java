7a3cbf436a355f739459edbf152108958b527841
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-31 23:28:53 +0200

    Implemented ImageFilePartBucketNameResolverSupport and written tests for it

--- src/test/java/com/odeyalo/sonata/harmony/config/aws/support/resolver/ImageFilePartBucketNameResolverSupportTest.java
+++ src/test/java/com/odeyalo/sonata/harmony/config/aws/support/resolver/ImageFilePartBucketNameResolverSupportTest.java
@@ -1,5 +1,6 @@
 package com.odeyalo.sonata.harmony.config.aws.support.resolver;
 
+import com.odeyalo.sonata.harmony.config.aws.support.ImageBucketNameSupplier;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -12,8 +13,19 @@ import testing.spring.web.FilePartStub;
 import java.io.IOException;
 
 class ImageFilePartBucketNameResolverSupportTest {
+    ImageBucketNameSupplier IMAGE_BUCKET_SUPPLIER = () -> "mocked";
 
-    ImageFilePartBucketNameResolverSupport testable = new ImageFilePartBucketNameResolverSupport();
+    ImageFilePartBucketNameResolverSupport testable = new ImageFilePartBucketNameResolverSupport(IMAGE_BUCKET_SUPPLIER);
+
+    @Test
+    void shouldReturnBucketNameIfTheFileIsImage() throws IOException {
+        FilePartStub file = prepareImageFile();
+
+        testable.resolveBucketName(file)
+                .as(StepVerifier::create)
+                .expectNext(IMAGE_BUCKET_SUPPLIER)
+                .verifyComplete();
+    }
 
     @Test
     void shouldReturnEmptyMonoIfFileIsNotImage() throws IOException {
@@ -22,6 +34,17 @@ class ImageFilePartBucketNameResolverSupportTest {
         testable.resolveBucketName(filePart)
                 .as(StepVerifier::create)
                 .verifyComplete();
+    }
+
+    @NotNull
+    private static FilePartStub prepareImageFile() throws IOException {
+        var content = new ClassPathResource("./img/album-cover.png");
+
+        DefaultDataBufferFactory bufferFactory = new DefaultDataBufferFactory();
+
+        DefaultDataBuffer dataBuffer = bufferFactory.wrap(content.getContentAsByteArray());
+
+        return new FilePartStub(Flux.just(dataBuffer), content.contentLength(), "album-cover.png", "image");
     }
 
     @NotNull
