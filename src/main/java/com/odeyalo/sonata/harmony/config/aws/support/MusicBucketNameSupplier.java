5a6f3691ee8946d18cc4b0d015978d2ce6fe10c0
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-01 13:32:02 +0200

    Supply bucket for music files only.

---
+++ src/main/java/com/odeyalo/sonata/harmony/config/aws/support/MusicBucketNameSupplier.java
@@ -0,0 +1,8 @@
+package com.odeyalo.sonata.harmony.config.aws.support;
+
+/**
+ * Supply bucket for music files only.
+ */
+public interface MusicBucketNameSupplier extends BucketNameSupplier {
+
+}
