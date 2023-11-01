96b31287757d5222101caae53ea4680e037814ec
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-01 14:07:01 +0200

    Written shouldReturnBucketNameForTrackFile test and make it pass

--- src/main/java/com/odeyalo/sonata/harmony/config/aws/support/resolver/MusicFilePartBucketNameResolverSupport.java
+++ src/main/java/com/odeyalo/sonata/harmony/config/aws/support/resolver/MusicFilePartBucketNameResolverSupport.java
@@ -1,15 +1,25 @@
 package com.odeyalo.sonata.harmony.config.aws.support.resolver;
 
 import com.odeyalo.sonata.harmony.config.aws.support.BucketNameSupplier;
+import com.odeyalo.sonata.harmony.config.aws.support.MusicBucketNameSupplier;
+import com.odeyalo.sonata.harmony.support.utils.FilenameUtils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Mono;
 
 public class MusicFilePartBucketNameResolverSupport implements FilePartBucketNameResolverSupport {
+    private final MusicBucketNameSupplier musicBucketNameSupplier;
+
+    public MusicFilePartBucketNameResolverSupport(MusicBucketNameSupplier musicBucketNameSupplier) {
+        this.musicBucketNameSupplier = musicBucketNameSupplier;
+    }
 
     @Override
     @NotNull
     public Mono<BucketNameSupplier> resolveBucketName(@NotNull FilePart target) {
+        if ( FilenameUtils.isMusicFile(target.filename()) ) {
+            return Mono.just(musicBucketNameSupplier);
+        }
         return Mono.empty();
     }
 }
