692e1c34ccb04a50502d635549f6fa1634482dc4
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-01 19:22:39 +0200

    Created repository interface to work with AlbumCoverImageEntity

---
+++ src/main/java/com/odeyalo/sonata/harmony/repository/AlbumCoverImageRepository.java
@@ -0,0 +1,10 @@
+package com.odeyalo.sonata.harmony.repository;
+
+import com.odeyalo.sonata.harmony.entity.AlbumCoverImageEntity;
+import reactor.core.publisher.Mono;
+
+public interface AlbumCoverImageRepository extends BasicRepository<AlbumCoverImageEntity, Long> {
+
+    Mono<AlbumCoverImageEntity> findByAlbumId(String albumId);
+
+}
