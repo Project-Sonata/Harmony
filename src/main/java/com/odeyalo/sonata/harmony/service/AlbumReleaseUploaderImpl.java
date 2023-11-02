c4884060bea885ec92145275b8b1b7651a57c0d4
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-02 15:51:37 +0200

    Written basic implementation and basic tests

---
+++ src/main/java/com/odeyalo/sonata/harmony/service/AlbumReleaseUploaderImpl.java
@@ -0,0 +1,29 @@
+package com.odeyalo.sonata.harmony.service;
+
+import com.odeyalo.sonata.harmony.service.album.AlbumReleaseUploadingStatus;
+import com.odeyalo.sonata.harmony.service.album.UploadAlbumReleaseInfo;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import static com.odeyalo.sonata.harmony.service.album.AlbumReleaseUploadingStatus.Type.RECEIVED;
+
+public class AlbumReleaseUploaderImpl implements AlbumReleaseUploader {
+
+    @Override
+    @NotNull
+    public Flux<AlbumReleaseUploadingStatus> uploadAlbumRelease(@NotNull UploadAlbumReleaseInfo info,
+                                                                @NotNull Flux<FilePart> tracks,
+                                                                @NotNull Mono<FilePart> coverImage) {
+
+        Sinks.Many<AlbumReleaseUploadingStatus> eventPublisher = Sinks.many().unicast().onBackpressureBuffer();
+
+        eventPublisher.tryEmitNext(AlbumReleaseUploadingStatus.of(RECEIVED));
+
+        eventPublisher.tryEmitComplete();
+
+        return eventPublisher.asFlux();
+    }
+}
---
+++ src/test/java/com/odeyalo/sonata/harmony/service/AlbumReleaseUploaderImplTest.java
@@ -0,0 +1,44 @@
+package com.odeyalo.sonata.harmony.service;
+
+import com.odeyalo.sonata.harmony.service.album.AlbumReleaseUploadingStatus;
+import com.odeyalo.sonata.harmony.service.album.UploadAlbumReleaseInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import testing.spring.web.FilePartStub;
+
+import static com.odeyalo.sonata.harmony.service.album.AlbumReleaseUploadingStatus.Type.RECEIVED;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AlbumReleaseUploaderImplTest {
+
+    AlbumReleaseUploaderImpl testable = new AlbumReleaseUploaderImpl();
+
+
+    @Test
+    void shouldReturnEventsInTheOrder() {
+        var info = UploadAlbumReleaseInfo.of();
+        Flux<FilePart> tracks = Flux.just(new FilePartStub(Flux.empty()));
+        Mono<FilePart> coverImage = Mono.just(new FilePartStub(Flux.empty()));
+
+        // received
+        // validation
+        // image upload
+        // track upload
+        // album info upload
+        // finished_with_verification_required
+        // finished_verified
+
+        testable.uploadAlbumRelease(info, tracks, coverImage)
+                .as(StepVerifier::create)
+                .expectNextMatches(status -> compareStatusType(status, RECEIVED))
+                .verifyComplete();
+    }
+
+    private static boolean compareStatusType(AlbumReleaseUploadingStatus status, AlbumReleaseUploadingStatus.Type expected) {
+        return status.getType() == expected;
+    }
+}
