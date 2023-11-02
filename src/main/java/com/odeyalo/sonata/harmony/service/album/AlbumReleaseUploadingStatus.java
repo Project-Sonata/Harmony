3aabb37a06da6a463095bcdc90897ea6cf5ef689
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-02 15:49:53 +0200

    Represent the current status of uploading the album release

---
+++ src/main/java/com/odeyalo/sonata/harmony/service/album/AlbumReleaseUploadingStatus.java
@@ -0,0 +1,19 @@
+package com.odeyalo.sonata.harmony.service.album;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Represent the current status of uploading the album release
+ */
+@Value
+@AllArgsConstructor(staticName = "of")
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AlbumReleaseUploadingStatus {
+    Type type;
+
+    public enum Type {
+        RECEIVED
+    }
+}
