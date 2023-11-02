863c9310df25f04cf9f15e1215eaf8900352411c
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-02 15:50:19 +0200

    Empty UploadAlbumReleaseInfo. Used to contain info about album release that will be uploaded

---
+++ src/main/java/com/odeyalo/sonata/harmony/service/album/UploadAlbumReleaseInfo.java
@@ -0,0 +1,11 @@
+package com.odeyalo.sonata.harmony.service.album;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor(staticName = "of")
+@Builder
+public class UploadAlbumReleaseInfo {
+}
