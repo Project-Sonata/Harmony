9165051d24434e46c37162b521da88d7d57585f2
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-02 15:51:21 +0200

    Interface used to upload the album release

--- src/main/java/com/odeyalo/sonata/harmony/service/AlbumReleaseUploader.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/AlbumReleaseUploader.java
@@ -1,4 +1,19 @@
 package com.odeyalo.sonata.harmony.service;
 
+import com.odeyalo.sonata.harmony.service.album.AlbumReleaseUploadingStatus;
+import com.odeyalo.sonata.harmony.service.album.UploadAlbumReleaseInfo;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Interface is used to upload the album release
+ */
 public interface AlbumReleaseUploader {
+
+    @NotNull
+    Flux<AlbumReleaseUploadingStatus> uploadAlbumRelease(@NotNull UploadAlbumReleaseInfo info,
+                                                         @NotNull Flux<FilePart> tracks,
+                                                         @NotNull Mono<FilePart> coverImage);
 }
