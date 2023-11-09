88a6aa6d9174c589531589201a867efe355babee
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-09 14:38:06 +0200

    Added Converters to config

--- src/main/java/com/odeyalo/sonata/harmony/support/converter/external/SimplifiedTrackDtoConverter.java
+++ src/main/java/com/odeyalo/sonata/harmony/support/converter/external/SimplifiedTrackDtoConverter.java
@@ -2,10 +2,12 @@ package com.odeyalo.sonata.harmony.support.converter.external;
 
 import com.odeyalo.sonata.harmony.model.Track;
 import com.odeyalo.sonata.suite.brokers.events.album.data.SimplifiedTrackDto;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = ArtistContainerDtoConverter.class)
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = ArtistContainerDtoConverter.class)
 public interface SimplifiedTrackDtoConverter {
 
     @Mapping(target = "name", source = "trackName")
--- src/main/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverter.java
+++ src/main/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverter.java
@@ -3,13 +3,14 @@ package com.odeyalo.sonata.harmony.support.converter.external;
 import com.odeyalo.sonata.harmony.model.AlbumRelease;
 import com.odeyalo.sonata.suite.brokers.events.album.data.UploadedAlbumInfoDto;
 import org.mapstruct.AfterMapping;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {
         ArtistContainerDtoConverter.class,
         SimplifiedTrackDtoContainerConverter.class
-})
+}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface UploadedAlbumInfoDtoConverter {
 
     UploadedAlbumInfoDto toUploadedAlbumInfoDto(AlbumRelease release);
