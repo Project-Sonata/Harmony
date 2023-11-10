a1bc2d397fd66720c2573041ff2ec5345ef00300
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-10 17:23:01 +0200

    Added logging

--- src/main/java/com/odeyalo/sonata/harmony/service/event/kafka/support/ChainedSonataEventTopicNameResolver.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/event/kafka/support/ChainedSonataEventTopicNameResolver.java
@@ -1,7 +1,9 @@
 package com.odeyalo.sonata.harmony.service.event.kafka.support;
 
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
+import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -9,9 +11,11 @@ import java.util.List;
 
 /**
  * Uses chain of KafkaEventTopicNameResolverSupport<SonataEvent> and return first non-empty result
- *
+ * <p>
  * Uses SonataEvent as object to resolve from
  */
+@Component
+@Log4j2
 public class ChainedSonataEventTopicNameResolver implements KafkaEventTopicNameResolver<SonataEvent> {
     private final List<KafkaEventTopicNameResolverSupport<SonataEvent>> resolvers;
 
@@ -24,6 +28,7 @@ public class ChainedSonataEventTopicNameResolver implements KafkaEventTopicNameR
     public Mono<KafkaTopicNameSupplier> resolveTopicName(@NotNull SonataEvent from) {
         return Flux.fromIterable(resolvers)
                 .flatMap(resolver -> resolver.resolveTopicName(from))
-                .next();
+                .next()
+                .doOnNext(supplier -> log.info("Will supply the topic using: {}", supplier));
     }
 }
