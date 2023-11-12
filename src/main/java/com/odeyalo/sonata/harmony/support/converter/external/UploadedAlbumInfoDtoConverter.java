1ce812ba0d0364760caa23d575b8802f4d42c64d
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2023-11-12 21:56:22 +0200

    Now it can convert release date (#15)
    Now converter can convert release date before sending to Kafka
    Written tests

--- src/main/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverter.java
+++ src/main/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverter.java
@@ -1,24 +1,29 @@
 package com.odeyalo.sonata.harmony.support.converter.external;
 
 import com.odeyalo.sonata.harmony.model.AlbumRelease;
+import com.odeyalo.sonata.harmony.repository.r2dbc.support.release.ReleaseDateEncoder;
 import com.odeyalo.sonata.suite.brokers.events.album.data.UploadedAlbumInfoDto;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = {
         ArtistContainerDtoConverter.class,
         SimplifiedTrackDtoContainerConverter.class
 }, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
-public interface UploadedAlbumInfoDtoConverter {
+public abstract class UploadedAlbumInfoDtoConverter {
 
-    UploadedAlbumInfoDto toUploadedAlbumInfoDto(AlbumRelease release);
+    @Autowired
+    ReleaseDateEncoder<String> releaseDateEncoder;
+
+    public abstract UploadedAlbumInfoDto toUploadedAlbumInfoDto(AlbumRelease release);
 
     @AfterMapping
-    default void releaseDateEnhancer(@MappingTarget UploadedAlbumInfoDto.UploadedAlbumInfoDtoBuilder builder,
-                                     AlbumRelease release) {
-        builder.releaseDateAsString("CHANGE ME")
+    public void releaseDateEnhancer(@MappingTarget UploadedAlbumInfoDto.UploadedAlbumInfoDtoBuilder builder,
+                                    AlbumRelease release) {
+        builder.releaseDateAsString(releaseDateEncoder.encodeReleaseDate(release.getReleaseDate()))
                 .releaseDatePrecision(release.getReleaseDate().getPrecision().name());
     }
 }
--- src/test/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverterTest.java
+++ src/test/java/com/odeyalo/sonata/harmony/support/converter/external/UploadedAlbumInfoDtoConverterTest.java
@@ -1,43 +1,62 @@
 package com.odeyalo.sonata.harmony.support.converter.external;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.odeyalo.sonata.harmony.model.AlbumRelease;
+import com.odeyalo.sonata.harmony.model.ReleaseDate;
+import com.odeyalo.sonata.harmony.repository.r2dbc.support.release.FormattedString2ReleaseDateConverter;
+import com.odeyalo.sonata.harmony.repository.r2dbc.support.release.ReleaseDateDecoder;
+import com.odeyalo.sonata.harmony.repository.r2dbc.support.release.ReleaseDateEncoder;
+import com.odeyalo.sonata.harmony.support.converter.external.UploadedAlbumInfoDtoConverterTest.Config;
 import com.odeyalo.sonata.suite.brokers.events.album.data.UploadedAlbumInfoDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import testing.faker.AlbumReleaseFaker;
 import testing.spring.MapStructBeansBootstrapConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@Import(MapStructBeansBootstrapConfiguration.class)
+@Import({MapStructBeansBootstrapConfiguration.class, Config.class})
 class UploadedAlbumInfoDtoConverterTest {
 
     @Autowired
     UploadedAlbumInfoDtoConverter testable;
-    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
-    void toUploadedAlbumInfoDto() throws JsonProcessingException {
+    void toUploadedAlbumInfoDto() {
         var albumRelease = AlbumReleaseFaker.create().get();
 
         UploadedAlbumInfoDto result = testable.toUploadedAlbumInfoDto(albumRelease);
 
         assertThat(result).isNotNull();
 
-        String json = objectMapper.writeValueAsString(result);
-
-        System.out.println(json);
-
         assertThat(result.getName()).isEqualTo(albumRelease.getName());
         assertThat(result.getAlbumType().name()).isEqualTo(albumRelease.getAlbumType().name());
 
         assertThat(result.getArtists()).hasSize(albumRelease.getArtists().size());
         assertThat(result.getTracks()).hasSize(albumRelease.getTracks().size());
+    }
+
+    @Test
+    void shouldContainReleaseDate() {
+        ReleaseDate releaseDate = ReleaseDate.withDay(3, 8, 2002);
+        var albumRelease = AlbumReleaseFaker.create().releaseDate(releaseDate).get();
+        UploadedAlbumInfoDto result = testable.toUploadedAlbumInfoDto(albumRelease);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getReleaseDatePrecision()).isEqualTo(ReleaseDate.Precision.DAY.name());
+        assertThat(result.getReleaseDateAsString()).isEqualTo("2002-08-03");
+    }
+
+    @TestConfiguration
+    static class Config {
+
+        @Bean
+        public ReleaseDateEncoder<String> releaseDateDecoder() {
+            return new FormattedString2ReleaseDateConverter();
+        }
     }
 }
