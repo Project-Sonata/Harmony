3197634b85d52ecc984c8d71853a521f8da81b60
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-22 19:08:26 +0200

    Now using AbstractEventPublisher instead of implementing the interface

--- src/main/java/com/odeyalo/sonata/harmony/service/event/impl/AlbumUploadingFullyFinishedEventPublisher.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/event/impl/AlbumUploadingFullyFinishedEventPublisher.java
@@ -1,24 +1,18 @@
 package com.odeyalo.sonata.harmony.service.event.impl;
 
-import com.odeyalo.sonata.harmony.service.event.EventPublisher;
+import com.odeyalo.sonata.harmony.service.event.AbstractEventPublisher;
 import com.odeyalo.sonata.harmony.service.event.EventPublisherDelegate;
 import com.odeyalo.sonata.suite.brokers.events.album.AlbumUploadingFullyFinishedEvent;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 @Component
 @Log4j2
-public class AlbumUploadingFullyFinishedEventPublisher implements EventPublisher<AlbumUploadingFullyFinishedEvent> {
-    private final EventPublisherDelegate eventPublisherDelegate;
+public class AlbumUploadingFullyFinishedEventPublisher extends AbstractEventPublisher<AlbumUploadingFullyFinishedEvent> {
 
+    @Autowired
     public AlbumUploadingFullyFinishedEventPublisher(EventPublisherDelegate eventPublisherDelegate) {
-        this.eventPublisherDelegate = eventPublisherDelegate;
-        log.info("Using the following delegate to publish the events: {}", eventPublisherDelegate);
-    }
-
-    @Override
-    public Mono<Void> publishEvent(AlbumUploadingFullyFinishedEvent event) {
-        return eventPublisherDelegate.publishEvent(event);
+        super(eventPublisherDelegate);
     }
 }
