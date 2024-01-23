ba15645f98097cdd42234c08f5b336c95a408688
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-23 16:04:36 +0200

    Fixed bug when BasicAlbumInfoUploadedEvent was not supported and empty mono was returned

--- src/main/java/com/odeyalo/sonata/harmony/service/event/kafka/support/AlbumKafkaEventTopicNameResolverSupport.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/event/kafka/support/AlbumKafkaEventTopicNameResolverSupport.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.harmony.service.event.kafka.support;
 
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
 import com.odeyalo.sonata.suite.brokers.events.album.AlbumUploadingFullyFinishedEvent;
+import com.odeyalo.sonata.suite.brokers.events.album.BasicAlbumInfoUploadedEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -12,7 +13,8 @@ public class AlbumKafkaEventTopicNameResolverSupport implements KafkaEventTopicN
     @Override
     @NotNull
     public Mono<KafkaTopicNameSupplier> resolveTopicName(@NotNull SonataEvent event) {
-        if ( event instanceof AlbumUploadingFullyFinishedEvent ) {
+        // bad practice, will update it when suite-commons 0.0.9 will be released
+        if ( event instanceof AlbumUploadingFullyFinishedEvent || event instanceof BasicAlbumInfoUploadedEvent ) {
             return Mono.just(StaticKafkaTopicNameSupplier.create("albums-event-warehouse")); // Should be changed
         }
         return Mono.empty();
