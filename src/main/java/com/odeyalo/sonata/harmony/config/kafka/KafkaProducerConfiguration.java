d25eaab6511d85c3396205613e99ba4cc35ba971
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-28 18:52:11 +0200

    Renamed to KafkaProducerConfiguration

--- src/main/java/com/odeyalo/sonata/harmony/config/kafka/KafkaProducerConfiguration.java
+++ src/main/java/com/odeyalo/sonata/harmony/config/kafka/KafkaProducerConfiguration.java
@@ -1,9 +1,9 @@
 package com.odeyalo.sonata.harmony.config.kafka;
 
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
-import com.odeyalo.sonata.suite.brokers.events.album.data.AlbumFullyUploadedInfo;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
@@ -14,12 +14,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConfiguration {
+public class KafkaProducerConfiguration {
 
     @Bean
-    public ReactiveKafkaProducerTemplate<String, SonataEvent> kafkaSender() {
+    public ReactiveKafkaProducerTemplate<String, SonataEvent> kafkaSender(KafkaProperties kafkaProperties) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
