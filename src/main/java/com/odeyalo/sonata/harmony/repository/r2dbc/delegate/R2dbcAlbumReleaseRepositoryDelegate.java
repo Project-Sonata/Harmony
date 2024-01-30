90fa1e007f12d14f2b2909434ff5b27f16b132c4
odeyalo <theprogrammerinfuture@gmail.com>
2024-01-30 22:08:56 +0200

    Added AlbumReleaseUpdateOperations

--- src/main/java/com/odeyalo/sonata/harmony/repository/r2dbc/delegate/R2dbcAlbumReleaseRepositoryDelegate.java
+++ src/main/java/com/odeyalo/sonata/harmony/repository/r2dbc/delegate/R2dbcAlbumReleaseRepositoryDelegate.java
@@ -1,9 +1,15 @@
 package com.odeyalo.sonata.harmony.repository.r2dbc.delegate;
 
 import com.odeyalo.sonata.harmony.entity.AlbumReleaseEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface R2dbcAlbumReleaseRepositoryDelegate extends R2dbcRepository<AlbumReleaseEntity, Long> {
+
+    @Query("update album_releases set duration_ms= :durationMs where id= :id")
+    Mono<AlbumReleaseEntity> updateDuration(Long id, Long durationMs);
+
 }
--- src/main/java/com/odeyalo/sonata/harmony/repository/r2dbc/delegate/R2dbcSimplifiedTrackRepositoryDelegate.java
+++ src/main/java/com/odeyalo/sonata/harmony/repository/r2dbc/delegate/R2dbcSimplifiedTrackRepositoryDelegate.java
@@ -2,11 +2,13 @@ package com.odeyalo.sonata.harmony.repository.r2dbc.delegate;
 
 import com.odeyalo.sonata.harmony.entity.SimplifiedTrackEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 /**
  * Delegate interface for SimplifiedTrackEntity entity
  */
+@Repository
 public interface R2dbcSimplifiedTrackRepositoryDelegate extends R2dbcRepository<SimplifiedTrackEntity, Long> {
 
     Flux<SimplifiedTrackEntity> findAllByAlbumId(Long id);
