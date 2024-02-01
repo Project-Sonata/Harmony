c9f27216ec3f886e13871d625f3bc7cb74d4bca1
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-01 16:19:34 +0200

    Changed Collection<EventListener> to List to add support for order, added logging

--- src/main/java/com/odeyalo/sonata/harmony/service/event/ListenerInvocationCapableEventManager.java
+++ src/main/java/com/odeyalo/sonata/harmony/service/event/ListenerInvocationCapableEventManager.java
@@ -2,31 +2,35 @@ package com.odeyalo.sonata.harmony.service.event;
 
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Capable to auto-invoke registered listeners.
  */
 @Component
+@Log4j2
 public class ListenerInvocationCapableEventManager implements EventListenerRegistry {
-    private final Collection<EventListener> listeners;
+    private final List<EventListener> listeners;
     private final EventSource<SonataEvent> eventSource;
 
-    public ListenerInvocationCapableEventManager(Collection<EventListener> listeners, EventSource<SonataEvent> eventSource) {
-        this.listeners = new ArrayList<>(listeners);
+    public ListenerInvocationCapableEventManager(List<EventListener> listeners, EventSource<SonataEvent> eventSource) {
+        this.listeners = listeners;
         this.eventSource = eventSource;
     }
 
     @PostConstruct
     public void startEventListening() {
+
         eventSource.getEvents()
                 .flatMap(event -> Flux.fromIterable(listeners)
+                        .doOnNext(listener -> log.debug("Handling the {} with {}", event, listener))
                         .filter(listener -> listener.supports(event))
                         .flatMap(listener -> listener.handleEvent(event)))
                 .subscribeOn(Schedulers.parallel())
